b256700b0dc402e1bed2154df633388993e78e32
Rodolfo Recordon <rodolforecordon@gmail.com>
2021-10-29 21:01:53 -0300

    setting up sequelize

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     'no-console': 'off',
     'import/no-self-import': 'off',
     'class-methods-use-this': 'off',
+    'import/first': 'off',
   },
 };
---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  define: {},
+  dialectOptions: {
+    timezone: 'America/Sao_Paulo',
+  },
+  timezone: 'America/Sao_Paulo',
+};
