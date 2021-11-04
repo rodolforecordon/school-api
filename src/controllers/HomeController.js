677089d44f0875b12c9411cf434b49842bc5e5e1
rodolforecordon <rodolforecordon@gmail.com>
2021-11-04 00:39:10 -0300

    Student controller and routes / model edited

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Student from '../models/Student';
-
 class HomeController {
   async index(req, res) {
-    const newStudent = await Student.create({
-      first_name: 'Rodolfo',
-      last_name: 'Recordon',
-      email: 'rodolfo@gmail.com',
-      age: 33,
-      weight: 68,
-      height: 1.73,
-    });
-    res.json(newStudent);
+    res.json('Index');
   }
 }
 
--- src/models/Student.js
+++ src/models/Student.js
@@ -3,12 +3,65 @@ import Sequelize, { Model } from 'sequelize';
 export default class Student extends Model {
   static init(sequelize) {
     super.init({
-      first_name: Sequelize.STRING,
-      last_name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      age: Sequelize.INTEGER,
-      weight: Sequelize.FLOAT,
-      height: Sequelize.FLOAT,
+      first_name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'First name must be 3-255 long.',
+          },
+        },
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Last name must be 3-255 long.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email already registered.',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Invalid email.',
+          },
+        },
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Age must be an integer.',
+          },
+        },
+      },
+      weight: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Weight must be a number.',
+          },
+        },
+      },
+      height: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Weight must be a number.',
+          },
+        },
+      },
     }, {
       sequelize,
     });
